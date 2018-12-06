fd26275c62584df37ff2d7f72cfd43d6cadb3f30
MatteoCasasanta <m.casasnta@fingerlinks.org>
2018-12-07 00:43:19 +0100

    abbiamo dato una colorata

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { MovieHeaderComponent } from './main/movie-header/movie-header.component';
+import { ArticlesComponent } from './main/articles/articles.component';
+import {AppRoutingModule} from './main/app-routing.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatDividerModule} from '@angular/material';
+import { CategoriesComponent } from './main/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    MovieHeaderComponent,
+    ArticlesComponent,
+    CategoriesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+
+    //Material
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
